3c25e493de14a5f9eb5c8487d3549b202299cf23
Tomáš Bouda <email@tomasbouda.cz>
2019-06-13 10:06:02 +0200

    Source folder

---
+++ src/TomLabs.ProgEx/TomLabs.ProgEx.Snake.CLI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace TomLabs.ProgEx.Snake.CLI
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var s = new Snaker(20, 20, 5);
+			int pause = 400;
+
+			var key = ConsoleKey.End;
+			do
+			{
+				s.Init();
+				while (key != ConsoleKey.Escape && !s.GameOver)
+				{
+					if (Console.KeyAvailable)
+					{
+						key = Console.ReadKey().Key;
+						switch (key)
+						{
+							case ConsoleKey.UpArrow: s.Steering = Core.Direction.Up; break;
+							case ConsoleKey.DownArrow: s.Steering = Core.Direction.Down; break;
+							case ConsoleKey.LeftArrow: s.Steering = Core.Direction.Left; break;
+							case ConsoleKey.RightArrow: s.Steering = Core.Direction.Right; break;
+						}
+					}
+
+					s.Cycle();
+					Thread.Sleep(pause - s.Score / 2);
+				}
+			} while (Console.ReadKey().Key == ConsoleKey.Enter);
+
+			Console.ReadKey();
+		}
+	}
+}
